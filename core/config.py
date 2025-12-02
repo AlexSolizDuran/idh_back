c12d4f82c5f9d45fb8fc63cf08fcc27440d52bfc
AlexSolizDuran <solizelex1999@gmail.com>
2025-12-02 10:54:19 -0400

    base

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     Configuración de la aplicación cargada desde variables de entorno.
     '''
     """
-    DATABASE_URL: str = "postgresql://neondb_owner:npg_KoxG7FCzI8sP@@ep-tiny-butterfly-afvunra9.c-2.aws.neon.tech/neondb?sslmode=require"
+    DATABASE_URL: str = "postgresql://neondb_owner:npg_KoxG7FCzI8sP@ep-tiny-butterfly-afvunra9-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     JWT_SECRET_KEY: str = "clave_secreta_super_segura_demo_123"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
